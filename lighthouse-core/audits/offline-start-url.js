7bbdf45b6d0f39969c6a90e5fb2417773f700934
Connor Clark <cjamcl@google.com>
2019-11-18 16:29:53 -0800

    lint

--- lighthouse-core/audits/offline-start-url.js
+++ lighthouse-core/audits/offline-start-url.js
@@ -59,13 +59,14 @@ class OfflineStartUrl extends Audit {
     // The StartUrl artifact provides no explanation if a response was received from a service
     // worker with a non-200 status code. In that case, this audit provides its own explanation.
     // In all other cases it defers to the artifact explanation.
-    let explanation = artifacts.StartUrl.explanation;
+    const explanation = artifacts.StartUrl.explanation;
     // if (!explanation && artifacts.StartUrl.statusCode !== -1 && !hasOfflineStartUrl) {
+    // eslint-disable-next-line max-len
     //   explanation = `Error loading ${artifacts.StartUrl.url} in Service Worker, got status code ${artifacts.StartUrl.statusCode}`;
     // }
     if (!explanation && artifacts.StartUrl.statusCode !== -1 && !hasOfflineStartUrl) {
-      warnings.push(
-        `Error loading ${artifacts.StartUrl.url} in Service Worker, got status code ${artifacts.StartUrl.statusCode}`);
+      // eslint-disable-next-line max-len
+      warnings.push(`Error loading ${artifacts.StartUrl.url} in Service Worker, got status code ${artifacts.StartUrl.statusCode}`);
     }
 
     return {
